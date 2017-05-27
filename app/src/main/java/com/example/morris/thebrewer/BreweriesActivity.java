5c1b1e337da563f1192a808bb3b3b8038b6cf791
Morris <mburumorris@gmail.com>
2017-05-27 14:03:11 +0300

    enable users to pass data between activities

---
+++ app/src/main/java/com/example/morris/thebrewer/BreweriesActivity.java
@@ -0,0 +1,20 @@
+package com.example.morris.thebrewer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class BreweriesActivity extends AppCompatActivity {
+    private TextView mLocationTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_breweries);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the Breweries in " + location);
+    }
+}
--- app/src/main/java/com/example/morris/thebrewer/MainActivity.java
+++ app/src/main/java/com/example/morris/thebrewer/MainActivity.java
@@ -1,41 +1,42 @@
 package com.example.morris.thebrewer;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindBreweryButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mFindBreweryButton  = (Button) findViewById(R.id.findBreweryButton);
+
+        mFindBreweryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, BreweriesActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+
+
+            }
+        });
+
+
 
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
